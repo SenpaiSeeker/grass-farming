19f99085bb98aa45ead5b74b392ca2d0da2cdfa8
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2024-11-30 11:32:02 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,23 +1,22 @@
 function auto() {
     python3 grass_freeproxy.py &
-    GRASS_PID=$!
-    echo "grass_freeproxy.py running with PID: $GRASS_PID"
+    PID=$!
+    echo "running with PID: $PID"
 }
 
-function stop_grass() {
-    if [ ! -z "$GRASS_PID" ]; then
-        echo "Stopping grass_freeproxy.py with PID: $GRASS_PID"
-        kill -9 $GRASS_PID
+function stop() {
+    if [ ! -z "$PID" ]; then
+        echo "Stopping grass_freeproxy.py with PID: $PID"
+        kill -9 $PID
         clear
     else
-        echo "No grass_freeproxy.py process found to stop."
+        echo "No process found to stop."
     fi
 }
 
 while true; do
     echo "Starting auto process..."
     auto
-    RANDOM_SLEEP=$((RANDOM % 360 * 10))
-    sleep $RANDOM_SLEEP
-    stop_grass
+    sleep $((RANDOM % 360 * 10))
+    stop
 done
