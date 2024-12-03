95d855604db595eacaebc6eaee32476432c0deb4
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2024-12-03 13:56:30 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,36 +1,22 @@
-#!/bin/bash
-
-LOG_FILE="script.log"
-
 function auto() {
-    echo "Starting script..."
-    python3 Script.py >> "$LOG_FILE" 2>&1 &
+    python3 grass_freeproxy.py &
     PID=$!
-    echo "Script running with PID: $PID"
+    echo "running with PID: $PID"
 }
 
 function stop() {
-    if [ ! -z "$PID" ] && ps -p $PID > /dev/null; then
-        echo "Stopping script with PID: $PID"
+    if [ ! -z "$PID" ]; then
+        echo "Stopping with PID: $PID"
         kill -9 $PID
-        wait $PID 2>/dev/null
+        clear
     else
-        echo "No running process found to stop."
+        echo "No process found to stop."
     fi
 }
 
-# Loop utama
 while true; do
-    echo "Monitoring log for 'Delay 6 minutes'..."
+    echo "Starting auto process..."
     auto
-
-    # Pantau log
-    while true; do
-        if grep -q "Delay 6 minutes" "$LOG_FILE"; then
-            echo "Log detected: 'Delay 6 minutes'. Restarting process..."
-            stop
-            break
-        fi
-        sleep 1 # Hindari penggunaan CPU tinggi
-    done
+    sleep $((RANDOM % 360 * 10 * 8))
+    stop
 done
