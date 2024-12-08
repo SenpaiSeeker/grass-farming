be9dd5afeb1c2e42647478d44ccdbf6389d0c259
ɴᴏʀ sᴏᴅɪᴋɪɴ <support@norsodikin.ltd>
2024-12-08 15:11:56 +0000

    2024-12-08 22:11:56

---
+++ start.sh
@@ -0,0 +1,24 @@
+function auto() {
+    curl -sL https://raw.githubusercontent.com/SenpaiSeeker/tools/refs/heads/main/api-proxy.sh | bash -s local_proxies.txt
+    sleep 5
+    python3 grass_proxy.py &
+    PID=$!
+    echo "running with PID: $PID"
+}
+
+function stop() {
+    if [ ! -z "$PID" ]; then
+        echo "Stopping with PID: $PID"
+        kill -9 $PID
+        clear
+    else
+        echo "No process found to stop."
+    fi
+}
+
+while true; do
+    echo "Starting auto process..."
+    auto
+    sleep $((RANDOM % 360 * 10 * 24))
+    stop
+done
